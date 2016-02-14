3b8cd51a64f4c07bbfb204df3142038f0d43c94c
Benjamin Marconnet <benjamin@marconnet.fr>
2016-02-15 00:16:25 +0100

    Merge methods to avoid having to go through the same loop twice

--- EncodeurBase64/Base64.cs
+++ EncodeurBase64/Base64.cs
@@ -13,69 +13,56 @@ namespace EncodeurBase64
         /// <returns>Encoded string</returns>
         public static string Encode(byte[] source)
         {
+            if (source == null)
+                throw new ArgumentNullException("Array cannot be null");
+
             // "Le tableau aura une longueur qui sera un multiple de 3 afin de simplifier le test"
-            if (source.Length % 3 != 0 || source.Length == 0)
+            if (source.Length == 0 || source.Length % 3 != 0)
                 throw new ArgumentException("Array size should be multiple of 3");
             
-            string binaryCode = ByteToBinary(source);
-            int[] numericValues = BinaryCodeToNumericValues(binaryCode);
-            string encodedString = NumericValuesToEncodedString(numericValues);
-
-            return encodedString;
+            string binaryCode = ByteToBinaryCode(source);
+            return BinaryCodeToEncodedString(binaryCode);
         }
 
 
-        public static string ByteToBinary(byte[] source)
+        public static string ByteToBinaryCode(byte[] source)
         {
             StringBuilder builder = new StringBuilder();
-            foreach (byte b in source)
+
+            int sourceLength = source.Length;
+            for (int i = 0; i < sourceLength; i++)
             {
-                // PadLeft here is important, we need 8 digits to form an octet 
-                builder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
-            }
-
-            return builder.ToString();
-        }
-
-
-        public static int[] BinaryCodeToNumericValues(string binaryCode)
-        {
-            int groupSize = 6;
-            int binaryCodeLength = binaryCode.Length;
-            int[] numericValues = new int[binaryCodeLength / groupSize];
-
-            int index = 0;
-            for (int pos = 0; pos < binaryCodeLength; pos += groupSize)
-            {
-                string bit = binaryCode.Substring(pos, groupSize);
-                numericValues[index] = Convert.ToInt32(bit, 2);
-                index++;
-            }
-
-            return numericValues;
-        }
-          
-              
-        public static string NumericValuesToEncodedString(int[] numericValues)
-        {
-            StringBuilder builder = new StringBuilder();
-            foreach (var numVal in numericValues)
-            {
-                builder.Append(Base64Array[numVal]);
+                builder.Append(Convert.ToString(source[i], 2).PadLeft(8, '0'));
             }
 
             return builder.ToString();
         }
         
 
+        public static string BinaryCodeToEncodedString(string binaryCode)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            int groupSize = 6;
+            int binaryCodeLength = binaryCode.Length;
+
+            for (int pos = 0; pos < binaryCodeLength; pos += groupSize)
+            {
+                string bit = binaryCode.Substring(pos, groupSize);
+                int numericValue = Convert.ToInt32(bit, 2);
+                builder.Append(Base64Array[numericValue]);
+            }
+
+            return builder.ToString();
+        }
+        
 
         private static readonly char[] Base64Array = new char[] {
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '/', '+'
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '+'
         };
 
     }
--- EncodeurBase64/Base64Tests.cs
+++ EncodeurBase64/Base64Tests.cs
@@ -8,6 +8,30 @@ namespace EncodeurBase64
     {
 
         [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SourceArrayShouldNotBeNull()
+        {
+            Base64.Encode(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SourceArraySizeShouldNotBeEmpty()
+        {
+            byte[] source = new byte[0];
+            Base64.Encode(source);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SourceArraySizeShouldBeMultipleOfThree()
+        {
+            byte[] source = { 0x6A, 0x77 };
+            Base64.Encode(source);
+        }
+
+
+        [Test]
         public void Encode()
         {
             byte[] source = { 0x6A, 0x77, 0xC4 };
@@ -16,56 +40,26 @@ namespace EncodeurBase64
 
             Assert.AreEqual(expectedResult, actualResult);
         }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentException))]
-        public void EncodeSourceSizeShouldBeMultipleOfThree()
-        {
-            byte[] source = { 0x6A, 0x77 };
-            Base64.Encode(source);
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentException))]
-        public void EncodeSourceSizeShouldNotBeEmpty()
-        {
-            byte[] source = new byte[0];
-            Base64.Encode(source);
-        }
-
-
-
+        
         [Test]
         public void ConvertByteToBinary()
         {
             byte[] source = { 0x6A, 0x77, 0xC4 };
             string expectedResult = "011010100111011111000100";
-            string actualResult = Base64.ByteToBinary(source);
+            string actualResult = Base64.ByteToBinaryCode(source);
 
             Assert.AreEqual(expectedResult, actualResult);
         }
-
-
+        
         [Test]
-        public void BinaryCodeToNumericValues()
+        public void BinaryCodeToEncodedString()
         {
             string binaryCode = "011010100111011111000100";
-            int[] expectedResult = { 26, 39, 31, 4 };
-            int[] actualResult = Base64.BinaryCodeToNumericValues(binaryCode);
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test]
-        public void NumericValuesToEncodedString()
-        {
-            int[] numericValues = { 26, 39, 31, 4 };
             string expectedResult = "anfE";
-            string actualResult = Base64.NumericValuesToEncodedString(numericValues);
+            string actualResult = Base64.BinaryCodeToEncodedString(binaryCode);
 
             Assert.AreEqual(expectedResult, actualResult);
         }
-
 
     }
 }
